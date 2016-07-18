0484cfd04acddc385b1ad528d10fa5a89fc4d422
Richard Drew <rdrew@fraedom.com>
2016-07-19 09:53:47 +1200

    Added more inline middleware examples

--- Startup.cs
+++ Startup.cs
@@ -14,15 +14,59 @@ namespace MiddlewareExamples
         }
         
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {			
-           // Simple path mapped to a builder branch
+        {	
+            // Inline middleware, runs a command and then has the option to allow execution (passthrough)
+            //  to the next delegate in the pipeline (app.Use()).
+            app.Use((context, next) =>
+            {
+                // Add an arbitrary value to the response
+                context.Response.Headers.Add("X-I-Am-The-Man", "true");
+
+                // Call the next delegate/middleware in the pipeline
+                return next();
+            });
+
+            // Inline middleware that branches the pipeline based on the logic suplied
+            // example 1 - the path '/my-heartbeat' is matched the branch will be run.
+            // This is the final step in the chain, there's no next delegate so if you
+            // put anything below it will not be executed as part of the pipeline (app.Map()).
             app.Map(
-                new PathString("/heartbeat"),
-                branch => branch.Run(async ctx =>
+                new PathString("/my-heartbeat"),
+                branch => branch.Run(async context =>
                 {
-                    await ctx.Response.WriteAsync("Boom, Boom, Boom!");
+                    await context.Response.WriteAsync("Boom, Boom, Boom!");
                 })
             );
+
+            // example 2 - the path '/your-heartbeat' is matched the branch will be run.
+            // This is the final step in the chain, there's no next delegate so if you
+            // put anything below it will not be executed as part of the pipeline (app.Map()).
+            app.Map(
+                new PathString("/your-heartbeat"),
+                branch => branch.Run(async context =>
+                {
+                    await context.Response.WriteAsync("Thud, Thud, Thud!");
+                })
+            );     
+
+            // example 3 - a querystring value of 'my-thing' is matched the branch will be run.
+            // This is the final step in the chain, there's no next delegate so if you
+            // put anything below it will not be executed as part of the pipeline (app.MapWhen()).
+            app.MapWhen(context => {
+                return context.Request.Query.ContainsKey("my-thing");
+            }, branch => branch.Run(async context =>
+               {
+                    await context.Response.WriteAsync("This is my thing!");
+               })
+            );       
+
+            // Inline middleware as a catch-all. No path string so anything that doesn't match
+            // the rules above will come here. This is the final step in the chain, there's no next 
+            // delegate so if you put anything below it will not be executed as part of the pipeline (app.Run())
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("This is the catch-all handler.");
+            });
         }
     }
 }
